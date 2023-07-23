b496fa356909e6e1ed8386efa154611a1cafd4f3
Mylo Cyrus <irahiggins740@gmail.com>
2023-07-23 13:14:45 -0700

    Add files via upload

---
+++ hooters.js.jl.py
@@ -0,0 +1,13 @@
+alien: Hey baby
+alien: Where you at?
+US/ME/USER:I'm at the oyo rn on flamingo at hooters...I've got a couple beers going and just waiting on My Cesar salad. Just come to the bar and ask for Mylo.
+alien: Sorry it tropicana
+repeat:Oyo on Tropicana. We ain't got any oyo on main St. Duh
+change:Tell them your name is Kila
+repeat:Wanna meet
+change: I'm waiting for u at the oyo. Hooters.on Tropicana. Ask for Mylo, say you are Kila
+r:Cool
+c:how much time you want?
+Cesar:Yes
+Sorry:
+
